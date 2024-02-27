07facca686139c2dd88b6a6e4f9fef0863552d37
Umesh Sharma <umeshsharma1495@gmal.com>
2024-02-28 01:48:14 +0530

    update ui changes

--- index.js
+++ index.js
@@ -3,41 +3,19 @@ const showDetails_2 = document.getElementById("box2");
 const showDetails_3 = document.getElementById("box3");
 
 showDetails_1.addEventListener("click", () => {
-  let x = document.getElementById("hide_a");
-  let y = document.getElementById("hide_b");
-  let z = document.getElementById("hide_c");
-
-  if ((x.style.display = "none")) {
-    x.style.display = "flex";
-    z.style.display = "none";
-    y.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
+  showDetails_1.classList.add("active_box");
+  showDetails_2.classList.remove("active_box");
+  showDetails_3.classList.remove("active_box");
 });
+
 showDetails_2.addEventListener("click", () => {
-  let x = document.getElementById("hide_a");
-  let y = document.getElementById("hide_b");
-  let z = document.getElementById("hide_c");
-
-  if ((y.style.display = "none")) {
-    y.style.display = "flex";
-    z.style.display = "none";
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
+  showDetails_1.classList.remove("active_box");
+  showDetails_2.classList.add("active_box");
+  showDetails_3.classList.remove("active_box");
 });
-showDetails_3.addEventListener("click", () => {
-  let x = document.getElementById("hide_a");
-  let y = document.getElementById("hide_b");
-  let z = document.getElementById("hide_c");
 
-  if ((z.style.display = "none")) {
-    z.style.display = "flex";
-    y.style.display = "none";
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
+showDetails_3.addEventListener("click", () => {
+  showDetails_1.classList.remove("active_box");
+  showDetails_2.classList.remove("active_box");
+  showDetails_3.classList.add("active_box");
 });
